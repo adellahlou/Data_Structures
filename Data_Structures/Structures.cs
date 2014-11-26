20b6128b28fb325d3cbdb2202333e61f7842a2b2
Adel Lahlou <adellahlou@users.noreply.github.com>
2014-11-26 13:57:27 -0600

    Gutted this file and dispersed into others.

---
+++ Data_Structures/Structures.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.CodeDom;
+using System.Diagnostics;
+using System.Reflection.Emit;
+using System.Dynamic;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.ComponentModel;
+using System.Net.Sockets;
+using System.IO;
+
+namespace DataStructures
+{
+
+
+	public class GraphNode
+	{
+
+	}
+
+	public class Graph
+	{
+
+	}
+		
+
+
+
+
+	public class RedBlackTreekNode<T>
+	{
+
+	}
+
+	public class RedBlackTree<T>
+	{
+
+	}
+
+
+	public class SkipList<T> 
+	{
+
+	}
+		
+
+}
